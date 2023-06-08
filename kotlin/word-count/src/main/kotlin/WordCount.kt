d10f5205c0557a22ff58420bd575f5b38eaf7061
Steve Bosman <stevebosman@gmail.com>
2023-06-08 23:49:09 +0100

    solution 1
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

---
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+object WordCount {
+    fun phrase(phrase: String): Map<String, Int> {
+        val normalisedPhrase = phrase.replace("[^a-zA-Z'\\d]+".toRegex(), " ").trim().lowercase(Locale.getDefault())
+        val words = normalisedPhrase.split(' ').map{ it.trim('\'') }
+        return words.associateWith { word -> words.count { it2 -> word == it2 } }
+    }
+}
