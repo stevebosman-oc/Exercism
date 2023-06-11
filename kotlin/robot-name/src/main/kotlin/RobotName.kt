9c13905e8be96c0d1d38856be32db8f566e9f98b
Steve Bosman <stevebosman@gmail.com>
2023-06-11 21:00:11 +0100

    Solution 1
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- kotlin/robot-name/src/main/kotlin/RobotName.kt
+++ kotlin/robot-name/src/main/kotlin/RobotName.kt
@@ -1,9 +1,28 @@
 class Robot {
+    companion object {
+        private val NAMES = mutableSetOf<String>()
+        private const val ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    private var mutableName = newName()
+
+    private fun newName(): String {
+        var newName: String
+        do {
+            newName = "${randomLetter()}${randomLetter()}${randomDigit()}${randomDigit()}${randomDigit()}"
+        } while (NAMES.contains(newName))
+        NAMES.add(newName)
+        return newName
+    }
+
+    private fun randomLetter() = ALPHABET[(0..25).random()]
+    private fun randomDigit() = (0..9).random()
 
     val name: String
-        get() = TODO("Implement this getter to complete the task")
+        get() = mutableName
 
     fun reset() {
-        TODO("Implement this function to complete the task")
+        val originalName = mutableName
+        mutableName = newName()
+        NAMES.remove(originalName)
     }
 }
