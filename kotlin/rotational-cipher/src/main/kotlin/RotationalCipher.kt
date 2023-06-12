b8577d0ba27fc3b8e59d0f0809e86b8b36899f3c
Steve Bosman <stevebosman@gmail.com>
2023-06-12 23:31:24 +0100

    raw solution RotationalCipher
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,8 +1,14 @@
-class RotationalCipher {
-
-    // TODO: Implement proper constructor
-
+class RotationalCipher(val offset: Int) {
     fun encode(text: String): String {
-        TODO("Implement this function to complete the task")
+        return String(text.map {
+            if (it.isLetter()) {
+                var raw = it + offset
+                if (it.isUpperCase() && raw > 'Z') raw -= 26
+                if (it.isLowerCase() && raw > 'z') raw -= 26
+                raw
+            } else {
+                it
+            }
+        }.toCharArray())
     }
 }
