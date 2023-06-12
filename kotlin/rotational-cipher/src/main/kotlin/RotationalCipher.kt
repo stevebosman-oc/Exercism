36a42785bf50961434bdf7ddeca1f4770d3a5d00
Steve Bosman <stevebosman@gmail.com>
2023-06-12 23:43:36 +0100

    Simplify
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,13 +1,12 @@
 class RotationalCipher(private val offset: Int) {
-    fun encode(text: String): String {
-        return text.map {
+    fun encode(text: String) =
+        text.map {
             when {
                 it.isLowerCase() -> it.rotate('a')
                 it.isUpperCase() -> it.rotate('A')
                 else -> it
             }
         }.joinToString("")
-    }
 
     private fun Char.rotate(base: Char) = (base + (this - base + offset) % 26)
 }
