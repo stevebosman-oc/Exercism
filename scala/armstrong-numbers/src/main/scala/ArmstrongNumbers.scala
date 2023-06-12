fe5faedbc05b78c3c8d40c528008f07143eb62b3
Steve Bosman <steve.bosman@opencastsoftware.com>
2023-06-12 15:41:20 +0100

    less terse solution
    Signed-off-by: Steve Bosman <steve.bosman@opencastsoftware.com>

--- scala/armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
+++ scala/armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -1,6 +1,9 @@
 import scala.math.pow
 
 object ArmstrongNumbers {
-  def isArmstrongNumber(i: Int): Boolean =
-    i == i.toString.map(_.asDigit).foldLeft(0)(_ + pow(_, i.toString.length).toInt)
+  def isArmstrongNumber(i: Int): Boolean = {
+    val order = i.toString.length
+    val digits = i.toString.map(_.asDigit)
+    i == digits.foldLeft(0)(_ + pow(_, order).toInt)
+  }
 }
